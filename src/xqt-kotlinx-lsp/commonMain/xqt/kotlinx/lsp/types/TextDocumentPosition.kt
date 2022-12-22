c855b833e644c405a4aa78a672b8d49df41518cb
Reece H. Dunn <msclrhd@gmail.com>
2022-12-22 13:46:46 +0000

    TextDocumentPosition

---
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/types/TextDocumentPosition.kt
@@ -0,0 +1,35 @@
+// Copyright (C) 2022 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.types
+
+import kotlinx.serialization.json.*
+import xqt.kotlinx.rpc.json.serialization.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonString
+
+/**
+ * Identifies a position in a text document.
+ *
+ * @since 1.0.0
+ */
+data class TextDocumentPosition(
+    override val uri: String,
+
+    /**
+     * The position inside the text document.
+     */
+    val position: Position
+) : TextDocumentIdentifier {
+    companion object : JsonSerialization<TextDocumentPosition> {
+        override fun serializeToJson(value: TextDocumentPosition): JsonObject = buildJsonObject {
+            put("uri", value.uri, JsonString)
+            put("position", value.position, Position)
+        }
+
+        override fun deserialize(json: JsonElement): TextDocumentPosition = when (json) {
+            !is JsonObject -> unsupportedKindType(json)
+            else -> TextDocumentPosition(
+                uri = json.get("uri", JsonString),
+                position = json.get("position", Position)
+            )
+        }
+    }
+}
