7c8b1b934ec971ae235bee55695d09c927a8e51a
Reece H. Dunn <msclrhd@gmail.com>
2023-01-03 18:15:20 +0000

    ErrorCodes

---
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/base/ResponseMessage.kt
@@ -0,0 +1,48 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.base
+
+import xqt.kotlinx.rpc.json.protocol.ErrorCode
+
+/**
+ * A number that indicates the error type that occurred.
+ *
+ * @since 1.0.0
+ */
+object ErrorCodes {
+    /**
+     * Invalid JSON was received by the server.
+     *
+     * An error occurred on the server while parsing the JSON text.
+     */
+    val ParseError: ErrorCode = ErrorCode(-32700)
+
+    /**
+     * Invalid method parameter(s).
+     */
+    val InternalError: ErrorCode = ErrorCode(-32603)
+
+    /**
+     * Invalid method parameter(s).
+     */
+    val InvalidParams: ErrorCode = ErrorCode(-32602)
+
+    /**
+     * The method does not exist / is not available.
+     */
+    val MethodNotFound: ErrorCode = ErrorCode(-32601)
+
+    /**
+     * The JSON sent is not a valid Request object.
+     */
+    val InvalidRequest: ErrorCode = ErrorCode(-32600)
+
+    /**
+     * Reserved for implementation-defined server-errors.
+     */
+    val ServerErrorStart: ErrorCode = ErrorCode(-32099)
+
+    /**
+     * Reserved for implementation-defined server-errors.
+     */
+    val ServerErrorEnd: ErrorCode = ErrorCode(-32000)
+}
---
+++ src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/base/ErrorCodes.kt
@@ -0,0 +1,54 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.test.base
+
+import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.lsp.base.ErrorCodes
+import xqt.kotlinx.rpc.json.protocol.ErrorCode
+import xqt.kotlinx.test.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@DisplayName("The LSP error codes")
+class TheLspErrorCodes {
+    @Test
+    @DisplayName("can serialize to JSON")
+    fun can_serialize_to_json() {
+        assertEquals("-32700", ErrorCode.serializeToJson(ErrorCodes.ParseError).toString())
+
+        assertEquals("-32603", ErrorCode.serializeToJson(ErrorCodes.InternalError).toString())
+        assertEquals("-32602", ErrorCode.serializeToJson(ErrorCodes.InvalidParams).toString())
+        assertEquals("-32601", ErrorCode.serializeToJson(ErrorCodes.MethodNotFound).toString())
+        assertEquals("-32600", ErrorCode.serializeToJson(ErrorCodes.InvalidRequest).toString())
+
+        assertEquals("-32099", ErrorCode.serializeToJson(ErrorCodes.ServerErrorStart).toString())
+        assertEquals("-32000", ErrorCode.serializeToJson(ErrorCodes.ServerErrorEnd).toString())
+    }
+
+    @Test
+    @DisplayName("can deserialize from JSON")
+    fun can_deserialize_from_json() {
+        assertEquals(ErrorCodes.ParseError, ErrorCode.deserialize(JsonPrimitive(-32700)))
+
+        assertEquals(ErrorCodes.InternalError, ErrorCode.deserialize(JsonPrimitive(-32603)))
+        assertEquals(ErrorCodes.InvalidParams, ErrorCode.deserialize(JsonPrimitive(-32602)))
+        assertEquals(ErrorCodes.MethodNotFound, ErrorCode.deserialize(JsonPrimitive(-32601)))
+        assertEquals(ErrorCodes.InvalidRequest, ErrorCode.deserialize(JsonPrimitive(-32600)))
+
+        assertEquals(ErrorCodes.ServerErrorStart, ErrorCode.deserialize(JsonPrimitive(-32099)))
+        assertEquals(ErrorCodes.ServerErrorEnd, ErrorCode.deserialize(JsonPrimitive(-32000)))
+    }
+
+    @Test
+    @DisplayName("map to JSON-RPC error codes")
+    fun map_to_json_rpc_error_codes() {
+        assertEquals(ErrorCode.ParseError, ErrorCodes.ParseError)
+
+        assertEquals(ErrorCode.InternalError, ErrorCodes.InternalError)
+        assertEquals(ErrorCode.InvalidParams, ErrorCodes.InvalidParams)
+        assertEquals(ErrorCode.MethodNotFound, ErrorCodes.MethodNotFound)
+        assertEquals(ErrorCode.InvalidRequest, ErrorCodes.InvalidRequest)
+
+        assertEquals(ErrorCode.ServerErrorRangeStart, ErrorCodes.ServerErrorStart)
+        assertEquals(ErrorCode.ServerErrorRangeEnd, ErrorCodes.ServerErrorEnd)
+    }
+}
