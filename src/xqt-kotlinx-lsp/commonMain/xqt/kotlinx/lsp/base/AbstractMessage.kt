51ff11df5ea742ec9d20107e7c64713d2d41a1c8
Reece H. Dunn <msclrhd@gmail.com>
2023-01-04 14:38:05 +0000

    Message

---
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/base/AbstractMessage.kt
@@ -0,0 +1,11 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.base
+
+import xqt.kotlinx.rpc.json.protocol.Message
+
+/**
+ * A general message as defined by JSON-RPC.
+ *
+ * @since 1.0.0
+ */
+typealias Message = Message
