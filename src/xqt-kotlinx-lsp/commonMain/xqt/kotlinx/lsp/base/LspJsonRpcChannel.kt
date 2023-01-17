821ec68be190eae2611ba721b3389b3d4928d8f3
Reece H. Dunn <msclrhd@gmail.com>
2023-01-17 20:50:52 +0000

    Define an LspJsonRpcChannel class.

---
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/base/LspJsonRpcChannel.kt
@@ -0,0 +1,54 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.base
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import xqt.kotlinx.rpc.json.io.*
+import xqt.kotlinx.rpc.json.protocol.JsonRpcChannel
+
+/**
+ * Handle LSP JSON-RPC header and content parts.
+ *
+ * The LSP specifies that JSON elements have an HTTP-like header section with a
+ * `Content-Length` and optional `Content-Type` header.
+ */
+class LspJsonRpcChannel(
+    private val input: BinaryInputChannel,
+    private val output: BinaryOutputChannel
+) : JsonRpcChannel {
+    override fun send(message: JsonElement) {
+        val body = message.toString().encodeToByteArray()
+        output.writeUtf8String("Content-Length: ${body.size}", lineEnding = LineEnding.CrLf)
+        output.writeUtf8String("", lineEnding = LineEnding.CrLf)
+        output.writeBytes(body)
+        output.flush()
+    }
+
+    override fun receive(): JsonElement? {
+        var contentLength = -1
+
+        var line = input.readUtf8Line(LineEnding.CrLf) ?: return null
+        while (line != "") {
+            if (line.startsWith("Content-Length: ")) {
+                val length = line.substringAfter("Content-Length: ")
+                contentLength = length.toIntOrNull() ?: -1
+            }
+            line = input.readUtf8Line(LineEnding.CrLf) ?: return null
+        }
+
+        if (contentLength == -1) {
+            return null
+        }
+
+        val body = input.readBytes(contentLength)
+        return when {
+            body.size != contentLength -> null
+            else -> Json.parseToJsonElement(body.decodeToString())
+        }
+    }
+
+    override fun close() {
+        input.close()
+        output.close()
+    }
+}
