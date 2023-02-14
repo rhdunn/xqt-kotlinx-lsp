8a15f2e58b39db7dcbf8b9fc2aadb7a95f3d2887
Reece H. Dunn <msclrhd@gmail.com>
2023-02-14 08:42:59 +0000

    ReferenceParams

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/References.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/References.kt
@@ -4,8 +4,43 @@ package xqt.kotlinx.lsp.textDocument
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
+import xqt.kotlinx.lsp.types.Position
+import xqt.kotlinx.lsp.types.TextDocumentPosition
 import xqt.kotlinx.rpc.json.serialization.*
 import xqt.kotlinx.rpc.json.serialization.types.JsonBoolean
+import xqt.kotlinx.rpc.json.serialization.types.JsonString
+
+/**
+ * The `textDocument/references` request parameters.
+ *
+ * @since 1.0.0
+ */
+data class ReferencesParams(
+    override val uri: String,
+    override val position: Position,
+
+    /**
+     * The reference context.
+     */
+    val context: ReferenceContext
+) : TextDocumentPosition {
+    companion object : JsonSerialization<ReferencesParams> {
+        override fun serializeToJson(value: ReferencesParams): JsonObject = buildJsonObject {
+            put("uri", value.uri, JsonString)
+            put("position", value.position, Position)
+            put("context", value.context, ReferenceContext)
+        }
+
+        override fun deserialize(json: JsonElement): ReferencesParams = when (json) {
+            !is JsonObject -> unsupportedKindType(json)
+            else -> ReferencesParams(
+                uri = json.get("uri", JsonString),
+                position = json.get("position", Position),
+                context = json.get("context", ReferenceContext)
+            )
+        }
+    }
+}
 
 /**
  * The reference context.
