4da117a2518a21d26cde40e7e88876e2e6c5b519
Reece H. Dunn <msclrhd@gmail.com>
2023-03-19 17:29:35 +0000

    Add a window server DSL property.

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/window/Window.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/window/Window.kt
@@ -1,6 +1,7 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.lsp.window
 
+import xqt.kotlinx.rpc.json.protocol.JsonRpcServer
 import xqt.kotlinx.rpc.json.protocol.Notification
 import kotlin.jvm.JvmInline
 
@@ -18,6 +19,19 @@ import kotlin.jvm.JvmInline
 value class WindowNotification(val notification: Notification)
 
 /**
+ * A method in the `window/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @param server the underlying JSON-RPC server
+ *
+ * @since 1.0.0
+ */
+@JvmInline
+value class WindowJsonRpcServer(val server: JsonRpcServer)
+
+/**
  * A notification in the `window/` namespace.
  *
  * Note that this does not check the namespace. Therefore, method DSLs need to
@@ -27,3 +41,14 @@ value class WindowNotification(val notification: Notification)
  */
 val Notification.window: WindowNotification
     get() = WindowNotification(this)
+
+/**
+ * A method in the `window/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @since 1.0.0
+ */
+val JsonRpcServer.window: WindowJsonRpcServer
+    get() = WindowJsonRpcServer(this)
