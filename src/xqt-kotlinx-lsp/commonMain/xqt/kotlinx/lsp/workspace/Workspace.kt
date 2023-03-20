f1f7310cf85855e914fbef63419358e5f9321c45
Reece H. Dunn <msclrhd@gmail.com>
2023-03-20 17:41:27 +0000

    Add a workspace server DSL property.

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/workspace/Workspace.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/workspace/Workspace.kt
@@ -1,6 +1,7 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.lsp.workspace
 
+import xqt.kotlinx.rpc.json.protocol.JsonRpcServer
 import xqt.kotlinx.rpc.json.protocol.Notification
 import kotlin.jvm.JvmInline
 
@@ -18,6 +19,19 @@ import kotlin.jvm.JvmInline
 value class WorkspaceNotification(val notification: Notification)
 
 /**
+ * A method in the `workspace/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @param server the underlying JSON-RPC server
+ *
+ * @since 1.0.0
+ */
+@JvmInline
+value class WorkspaceJsonRpcServer(val server: JsonRpcServer)
+
+/**
  * A notification in the `workspace/` namespace.
  *
  * Note that this does not check the namespace. Therefore, method DSLs need to
@@ -27,3 +41,14 @@ value class WorkspaceNotification(val notification: Notification)
  */
 val Notification.workspace: WorkspaceNotification
     get() = WorkspaceNotification(this)
+
+/**
+ * A method in the `workspace/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @since 1.0.0
+ */
+val JsonRpcServer.workspace: WorkspaceJsonRpcServer
+    get() = WorkspaceJsonRpcServer(this)
