fdc4f63c4236cc5e24ebcd93e949d0fdde975fa5
Reece H. Dunn <msclrhd@gmail.com>
2023-03-21 22:30:18 +0000

    Add a textDocument server DSL property.

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/TextDocument.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/TextDocument.kt
@@ -1,6 +1,7 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.lsp.textDocument
 
+import xqt.kotlinx.rpc.json.protocol.JsonRpcServer
 import xqt.kotlinx.rpc.json.protocol.Notification
 import kotlin.jvm.JvmInline
 
@@ -18,6 +19,19 @@ import kotlin.jvm.JvmInline
 value class TextDocumentNotification(val notification: Notification)
 
 /**
+ * A method in the `textDocument/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @param server the underlying JSON-RPC server
+ *
+ * @since 1.0.0
+ */
+@JvmInline
+value class TextDocumentJsonRpcServer(val server: JsonRpcServer)
+
+/**
  * A notification in the `textDocument/` namespace.
  *
  * Note that this does not check the namespace. Therefore, method DSLs need to
@@ -27,3 +41,14 @@ value class TextDocumentNotification(val notification: Notification)
  */
 val Notification.textDocument: TextDocumentNotification
     get() = TextDocumentNotification(this)
+
+/**
+ * A method in the `textDocument/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @since 1.0.0
+ */
+val JsonRpcServer.textDocument: TextDocumentJsonRpcServer
+    get() = TextDocumentJsonRpcServer(this)
