50d04c6f325128ae31d9f4dbacb65e3989fa3965
Reece H. Dunn <msclrhd@gmail.com>
2023-03-21 22:05:08 +0000

    Add a textDocument notification DSL property.

---
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/TextDocument.kt
@@ -0,0 +1,29 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.textDocument
+
+import xqt.kotlinx.rpc.json.protocol.Notification
+import kotlin.jvm.JvmInline
+
+/**
+ * A notification in the `textDocument/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method DSLs need to
+ * check the fully qualified method name.
+ *
+ * @param notification the underlying notification message
+ *
+ * @since 1.0.0
+ */
+@JvmInline
+value class TextDocumentNotification(val notification: Notification)
+
+/**
+ * A notification in the `textDocument/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method DSLs need to
+ * check the fully qualified method name.
+ *
+ * @since 1.0.0
+ */
+val Notification.textDocument: TextDocumentNotification
+    get() = TextDocumentNotification(this)
