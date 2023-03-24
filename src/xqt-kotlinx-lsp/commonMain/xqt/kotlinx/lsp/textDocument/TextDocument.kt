1b8329b235dbd305146b2f09e3674b60cad95eec
Reece H. Dunn <msclrhd@gmail.com>
2023-03-24 08:31:40 +0000

    Add a textDocument request DSL property.

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/TextDocument.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/TextDocument.kt
@@ -1,9 +1,23 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.lsp.textDocument
 
+import xqt.kotlinx.lsp.base.RequestMessage
 import xqt.kotlinx.rpc.json.protocol.JsonRpcServer
 import xqt.kotlinx.rpc.json.protocol.Notification
 import kotlin.jvm.JvmInline
+
+/**
+ * A request in the `textDocument/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method DSLs need to
+ * check the fully qualified method name.
+ *
+ * @param request the underlying request message
+ *
+ * @since 1.0.0
+ */
+@JvmInline
+value class TextDocumentRequest(val request: RequestMessage)
 
 /**
  * A notification in the `textDocument/` namespace.
@@ -62,6 +76,17 @@ value class TextDocumentNotification(val notification: Notification) {
  */
 @JvmInline
 value class TextDocumentJsonRpcServer(val server: JsonRpcServer)
+
+/**
+ * A request in the `textDocument/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method DSLs need to
+ * check the fully qualified method name.
+ *
+ * @since 1.0.0
+ */
+val RequestMessage.textDocument: TextDocumentRequest
+    get() = TextDocumentRequest(this)
 
 /**
  * A notification in the `textDocument/` namespace.
