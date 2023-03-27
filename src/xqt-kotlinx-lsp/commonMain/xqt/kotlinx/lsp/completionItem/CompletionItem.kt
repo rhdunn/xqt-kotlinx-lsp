4ec704ee915173bc2ed61ee3b1e2c4af087bd7da
Reece H. Dunn <msclrhd@gmail.com>
2023-03-27 08:46:05 +0100

    Add a completionItem server DSL property.

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/completionItem/CompletionItem.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/completionItem/CompletionItem.kt
@@ -2,6 +2,7 @@
 package xqt.kotlinx.lsp.completionItem
 
 import xqt.kotlinx.lsp.base.RequestMessage
+import xqt.kotlinx.rpc.json.protocol.JsonRpcServer
 import kotlin.jvm.JvmInline
 
 /**
@@ -26,6 +27,19 @@ value class CompletionItemRequest(val request: RequestMessage) {
 }
 
 /**
+ * A method in the `completionItem/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @param server the underlying JSON-RPC server
+ *
+ * @since 1.0.0
+ */
+@JvmInline
+value class CompletionItemJsonRpcServer(val server: JsonRpcServer)
+
+/**
  * A request in the `completionItem/` namespace.
  *
  * Note that this does not check the namespace. Therefore, method DSLs need to
@@ -35,3 +49,14 @@ value class CompletionItemRequest(val request: RequestMessage) {
  */
 val RequestMessage.completionItem: CompletionItemRequest
     get() = CompletionItemRequest(this)
+
+/**
+ * A method in the `completionItem/` namespace.
+ *
+ * Note that this does not check the namespace. Therefore, method wrappers need
+ * to specify the fully qualified method name.
+ *
+ * @since 1.0.0
+ */
+val JsonRpcServer.completionItem: CompletionItemJsonRpcServer
+    get() = CompletionItemJsonRpcServer(this)
