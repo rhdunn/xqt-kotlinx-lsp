66b146a8cc962d91327929cfc27225c674ca5654
Reece H. Dunn <msclrhd@gmail.com>
2023-03-30 07:55:38 +0100

    completionItem/resolve send request API

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/completionItem/Resolve.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/completionItem/Resolve.kt
@@ -1,8 +1,38 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.lsp.completionItem
 
+import kotlinx.serialization.json.JsonElement
 import xqt.kotlinx.lsp.textDocument.CompletionItem
 import xqt.kotlinx.rpc.json.protocol.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonIntOrString
+
+/**
+ * The response of a resolve request.
+ *
+ * @param id the request id
+ * @param result the result of the request
+ * @param error the error object in case the request failed
+ * @param jsonrpc the version of the JSON-RPC protocol
+ *
+ * @since 1.0.0
+ */
+data class ResolveResponse(
+    override val id: JsonIntOrString?,
+    override val result: CompletionItem?,
+    override val error: TypedErrorObject<JsonElement>?,
+    override val jsonrpc: String
+) : TypedResponseObject<CompletionItem?, JsonElement> {
+    companion object : TypedResponseObjectConverter<CompletionItem?, JsonElement> {
+        override fun convert(response: ResponseObject): TypedResponseObject<CompletionItem?, JsonElement> {
+            return ResolveResponse(
+                id = response.id,
+                result = response.result?.let { CompletionItem.deserialize(it) },
+                error = response.error,
+                jsonrpc = response.jsonrpc
+            )
+        }
+    }
+}
 
 /**
  * The request is sent from the client to the server to resolve additional information for
@@ -16,3 +46,23 @@ fun CompletionItemRequest.resolve(handler: CompletionItem.() -> CompletionItem) 
         request.sendResult(result, CompletionItem)
     }
 }
+
+/**
+ * The request is sent from the client to the server to resolve additional information for
+ * a given completion item.
+ *
+ * @param params the request parameters
+ * @param responseHandler the callback to process the response for the request
+ * @return the ID of the request
+ *
+ * @since 1.0.0
+ */
+fun CompletionItemJsonRpcServer.resolve(
+    params: CompletionItem,
+    responseHandler: (TypedResponseObject<CompletionItem?, JsonElement>.() -> Unit)? = null
+): JsonIntOrString = server.sendRequest(
+    method = CompletionItemRequest.RESOLVE,
+    params = CompletionItem.serializeToJson(params),
+    responseHandler = responseHandler,
+    responseObjectConverter = ResolveResponse
+)
--- src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/completionItem/CompletionItem.kt
+++ src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/completionItem/CompletionItem.kt
@@ -2,13 +2,14 @@
 package xqt.kotlinx.lsp.test.completionItem
 
 import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.lsp.base.ErrorCodes
+import xqt.kotlinx.lsp.base.InternalError
 import xqt.kotlinx.lsp.completionItem.completionItem
 import xqt.kotlinx.lsp.completionItem.resolve
 import xqt.kotlinx.lsp.test.base.testJsonRpc
 import xqt.kotlinx.lsp.textDocument.*
 import xqt.kotlinx.rpc.json.protocol.jsonRpc
 import xqt.kotlinx.rpc.json.protocol.request
-import xqt.kotlinx.rpc.json.serialization.jsonArrayOf
 import xqt.kotlinx.rpc.json.serialization.jsonObjectOf
 import xqt.kotlinx.rpc.json.serialization.types.JsonIntOrString
 import xqt.kotlinx.test.DisplayName
@@ -65,6 +66,103 @@ class CompletionItemDSL {
             ),
             client.receive()
         )
+    }
+
+    @Test
+    @DisplayName("supports sending completionItem/resolve requests")
+    fun supports_sending_resolve_requests() = testJsonRpc {
+        val id = client.completionItem.resolve(
+            params = CompletionItem(
+                label = "Lorem Ipsum",
+                kind = CompletionItemKind.Text
+            )
+        )
+        assertEquals(JsonIntOrString.IntegerValue(1), id)
+
+        assertEquals(
+            jsonObjectOf(
+                "jsonrpc" to JsonPrimitive("2.0"),
+                "method" to JsonPrimitive("completionItem/resolve"),
+                "id" to JsonPrimitive(1),
+                "params" to jsonObjectOf(
+                    "label" to JsonPrimitive("Lorem Ipsum"),
+                    "kind" to JsonPrimitive(1)
+                )
+            ),
+            server.receive()
+        )
+    }
+
+    @Test
+    @DisplayName("supports completionItem/resolve request callback receiving CompletionItem")
+    fun supports_resolve_request_callback_receiving_completion_item() = testJsonRpc {
+        var called = 0
+
+        client.completionItem.resolve(
+            params = CompletionItem(
+                label = "Lorem Ipsum",
+                kind = CompletionItemKind.Text
+            )
+        ) {
+            ++called
+
+            assertEquals(
+                CompletionItem(
+                    label = "Lorem Ipsum",
+                    kind = CompletionItemKind.Text
+                ),
+                result
+            )
+
+            assertEquals(null, error)
+        }
+
+        server.jsonRpc {
+            request {
+                completionItem.resolve {
+                    CompletionItem(
+                        label = "Lorem Ipsum",
+                        kind = CompletionItemKind.Text
+                    )
+                }
+            }
+        }
+
+        assertEquals(0, called, "The completionItem.resolve DSL handler should not have been called.")
+        client.jsonRpc {} // The "completionItem/resolve" response is processed by the handler callback.
+        assertEquals(1, called, "The completionItem.resolve DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports completionItem/resolve request callback receiving ErrorObject")
+    fun supports_resolve_request_callback_receiving_error_object() = testJsonRpc {
+        var called = 0
+
+        client.completionItem.resolve(
+            params = CompletionItem(
+                label = "Lorem Ipsum",
+                kind = CompletionItemKind.Text
+            )
+        ) {
+            ++called
+
+            assertEquals(null, result)
+
+            assertEquals(ErrorCodes.InternalError, error?.code)
+            assertEquals("Lorem ipsum", error?.message)
+        }
+
+        server.jsonRpc {
+            request {
+                completionItem.resolve {
+                    throw InternalError(message = "Lorem ipsum")
+                }
+            }
+        }
+
+        assertEquals(0, called, "The completionItem.resolve DSL handler should not have been called.")
+        client.jsonRpc {} // The "completionItem/resolve" response is processed by the handler callback.
+        assertEquals(1, called, "The completionItem.resolve DSL handler should have been called.")
     }
 
     // endregion
