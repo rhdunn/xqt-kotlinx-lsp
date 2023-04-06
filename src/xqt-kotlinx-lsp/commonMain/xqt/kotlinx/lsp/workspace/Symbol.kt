84c933af473baf314e3c951a1e8c091d86f62ab4
Reece H. Dunn <msclrhd@gmail.com>
2023-04-06 16:39:59 +0100

    Add a workspace/symbols send request API

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/workspace/Symbol.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/workspace/Symbol.kt
@@ -5,9 +5,13 @@ import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
 import xqt.kotlinx.lsp.textDocument.SymbolInformation
+import xqt.kotlinx.lsp.textDocument.SymbolInformationResponse
+import xqt.kotlinx.rpc.json.protocol.TypedResponseObject
 import xqt.kotlinx.rpc.json.protocol.params
+import xqt.kotlinx.rpc.json.protocol.sendRequest
 import xqt.kotlinx.rpc.json.protocol.sendResult
 import xqt.kotlinx.rpc.json.serialization.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonIntOrString
 import xqt.kotlinx.rpc.json.serialization.types.JsonString
 import xqt.kotlinx.rpc.json.serialization.types.JsonTypedArray
 
@@ -50,3 +54,41 @@ fun WorkspaceRequest.symbol(handler: WorkspaceSymbolParams.() -> List<SymbolInfo
         request.sendResult(result, SymbolInformationArray)
     }
 }
+
+/**
+ * The workspace symbol request is sent from the client to the server to list project-wide
+ * symbols matching the query string.
+ *
+ * @param params the request parameters
+ * @param responseHandler the callback to process the response for the request
+ * @return the ID of the request
+ *
+ * @since 1.0.0
+ */
+fun WorkspaceJsonRpcServer.symbol(
+    params: WorkspaceSymbolParams,
+    responseHandler: (TypedResponseObject<List<SymbolInformation>, JsonElement>.() -> Unit)? = null
+): JsonIntOrString = server.sendRequest(
+    method = WorkspaceRequest.SYMBOL,
+    params = WorkspaceSymbolParams.serializeToJson(params),
+    responseHandler = responseHandler,
+    responseObjectConverter = SymbolInformationResponse
+)
+
+/**
+ * The workspace symbol request is sent from the client to the server to list project-wide
+ * symbols matching the query string.
+ *
+ * @param query a non-empty query string
+ * @param responseHandler the callback to process the response for the request
+ * @return the ID of the request
+ *
+ * @since 1.0.0
+ */
+fun WorkspaceJsonRpcServer.symbol(
+    query: String,
+    responseHandler: (TypedResponseObject<List<SymbolInformation>, JsonElement>.() -> Unit)? = null
+): JsonIntOrString = symbol(
+    params = WorkspaceSymbolParams(query = query),
+    responseHandler = responseHandler
+)
--- src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/workspace/Workspace.kt
+++ src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/workspace/Workspace.kt
@@ -2,7 +2,10 @@
 package xqt.kotlinx.lsp.test.workspace
 
 import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.lsp.base.ErrorCodes
+import xqt.kotlinx.lsp.base.InternalError
 import xqt.kotlinx.lsp.test.base.testJsonRpc
+import xqt.kotlinx.lsp.workspace.workspace
 import xqt.kotlinx.lsp.workspace.*
 import xqt.kotlinx.rpc.json.protocol.jsonRpc
 import xqt.kotlinx.rpc.json.protocol.notification
@@ -89,7 +92,7 @@ class WorkspaceDSL {
     }
 
     // endregion
-    // region textDocument/didChangeWatchedFiles notification
+    // region workspace/didChangeWatchedFiles notification
 
     @Test
     @DisplayName("supports workspace/didChangeWatchedFiles notifications")
@@ -162,7 +165,7 @@ class WorkspaceDSL {
     }
 
     // endregion
-    // region textDocument/documentSymbol request
+    // region workspace/symbol request
 
     @Test
     @DisplayName("supports workspace/symbol requests")
@@ -205,6 +208,168 @@ class WorkspaceDSL {
             ),
             client.receive()
         )
+    }
+
+    @Test
+    @DisplayName("supports sending workspace/symbol requests using parameter objects")
+    fun supports_sending_symbol_requests_using_parameter_objects() = testJsonRpc {
+        val id = client.workspace.symbol(
+            params = WorkspaceSymbolParams(
+                query = "lorem"
+            )
+        )
+        assertEquals(JsonIntOrString.IntegerValue(1), id)
+
+        assertEquals(
+            jsonObjectOf(
+                "jsonrpc" to JsonPrimitive("2.0"),
+                "method" to JsonPrimitive("workspace/symbol"),
+                "id" to JsonPrimitive(1),
+                "params" to jsonObjectOf(
+                    "query" to JsonPrimitive("lorem")
+                )
+            ),
+            server.receive()
+        )
+    }
+
+    @Test
+    @DisplayName("supports workspace/symbol request callback receiving a result using parameter objects")
+    fun supports_symbol_request_callback_receiving_a_result_using_parameter_objects() = testJsonRpc {
+        var called = 0
+
+        client.workspace.symbol(
+            params = WorkspaceSymbolParams(
+                query = "lorem"
+            )
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(null, error)
+        }
+
+        server.jsonRpc {
+            request {
+                workspace.symbol {
+                    listOf()
+                }
+            }
+        }
+
+        assertEquals(0, called, "The workspace.symbol DSL handler should not have been called.")
+        client.jsonRpc {} // The "workspace/symbol" response is processed by the handler callback.
+        assertEquals(1, called, "The workspace.symbol DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports workspace/symbol request callback receiving an error using parameter objects")
+    fun supports_document_symbol_request_callback_receiving_an_error_using_parameter_objects() = testJsonRpc {
+        var called = 0
+
+        client.workspace.symbol(
+            params = WorkspaceSymbolParams(
+                query = "lorem"
+            )
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(ErrorCodes.InternalError, error?.code)
+            assertEquals("Lorem ipsum", error?.message)
+        }
+
+        server.jsonRpc {
+            request {
+                workspace.symbol {
+                    throw InternalError(message = "Lorem ipsum")
+                }
+            }
+        }
+
+        assertEquals(0, called, "The workspace.symbol DSL handler should not have been called.")
+        client.jsonRpc {} // The "workspace/symbol" response is processed by the handler callback.
+        assertEquals(1, called, "The workspace.symbol DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports sending workspace/symbol requests using function parameters")
+    fun supports_sending_document_symbol_requests_using_function_parameters() = testJsonRpc {
+        val id = client.workspace.symbol(
+            query = "lorem"
+        )
+        assertEquals(JsonIntOrString.IntegerValue(1), id)
+
+        assertEquals(
+            jsonObjectOf(
+                "jsonrpc" to JsonPrimitive("2.0"),
+                "method" to JsonPrimitive("workspace/symbol"),
+                "id" to JsonPrimitive(1),
+                "params" to jsonObjectOf(
+                    "query" to JsonPrimitive("lorem")
+                )
+            ),
+            server.receive()
+        )
+    }
+
+    @Test
+    @DisplayName("supports workspace/symbol request callback receiving a result using function parameters")
+    fun supports_document_symbol_request_callback_receiving_a_result_using_function_parameters() = testJsonRpc {
+        var called = 0
+
+        client.workspace.symbol(
+            query = "lorem"
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(null, error)
+        }
+
+        server.jsonRpc {
+            request {
+                workspace.symbol {
+                    listOf()
+                }
+            }
+        }
+
+        assertEquals(0, called, "The workspace.symbol DSL handler should not have been called.")
+        client.jsonRpc {} // The "workspace/symbol" response is processed by the handler callback.
+        assertEquals(1, called, "The workspace.symbol DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports workspace/symbol request callback receiving an error using function parameters")
+    fun supports_document_symbol_request_callback_receiving_an_error_using_function_parameters() = testJsonRpc {
+        var called = 0
+
+        client.workspace.symbol(
+            query = "lorem"
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(ErrorCodes.InternalError, error?.code)
+            assertEquals("Lorem ipsum", error?.message)
+        }
+
+        server.jsonRpc {
+            request {
+                workspace.symbol {
+                    throw InternalError(message = "Lorem ipsum")
+                }
+            }
+        }
+
+        assertEquals(0, called, "The workspace.symbol DSL handler should not have been called.")
+        client.jsonRpc {} // The "workspace/symbol" response is processed by the handler callback.
+        assertEquals(1, called, "The workspace.symbol DSL handler should have been called.")
     }
 
     // endregion
