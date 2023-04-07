09f7ab9bbc307ce0009db33e8d03e048eeebf000
Reece H. Dunn <msclrhd@gmail.com>
2023-04-07 11:02:03 +0100

    Add a textDocument/codeAction send request API

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/CodeAction.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/CodeAction.kt
@@ -8,9 +8,9 @@ import xqt.kotlinx.lsp.types.Command
 import xqt.kotlinx.lsp.types.Diagnostic
 import xqt.kotlinx.lsp.types.Range
 import xqt.kotlinx.lsp.types.TextDocumentIdentifier
-import xqt.kotlinx.rpc.json.protocol.params
-import xqt.kotlinx.rpc.json.protocol.sendResult
+import xqt.kotlinx.rpc.json.protocol.*
 import xqt.kotlinx.rpc.json.serialization.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonIntOrString
 import xqt.kotlinx.rpc.json.serialization.types.JsonTypedArray
 
 /**
@@ -83,6 +83,34 @@ data class CodeActionContext(
 private val CommandArray = JsonTypedArray(Command)
 
 /**
+ * The response of a code action request.
+ *
+ * @param id the request id
+ * @param result the result of the request
+ * @param error the error object in case the request failed
+ * @param jsonrpc the version of the JSON-RPC protocol
+ *
+ * @since 1.0.0
+ */
+data class CommandResponse(
+    override val id: JsonIntOrString?,
+    override val result: List<Command>,
+    override val error: TypedErrorObject<JsonElement>?,
+    override val jsonrpc: String
+) : TypedResponseObject<List<Command>, JsonElement> {
+    companion object : TypedResponseObjectConverter<List<Command>, JsonElement> {
+        override fun convert(response: ResponseObject): TypedResponseObject<List<Command>, JsonElement> {
+            return CommandResponse(
+                id = response.id,
+                result = response.result?.let { CommandArray.deserialize(it) } ?: listOf(),
+                error = response.error,
+                jsonrpc = response.jsonrpc
+            )
+        }
+    }
+}
+
+/**
  * The code action request is sent from the client to the server to compute commands for
  * a given text document and range.
  *
@@ -97,3 +125,55 @@ fun TextDocumentRequest.codeAction(handler: CodeActionParams.() -> List<Command>
         request.sendResult(result, CommandArray)
     }
 }
+
+/**
+ * The code action request is sent from the client to the server to compute commands for
+ * a given text document and range.
+ *
+ * The request is trigger when the user moves the cursor into an problem marker in the
+ * editor or presses the lightbulb associated with a marker.
+ *
+ * @param params the request parameters
+ * @param responseHandler the callback to process the response for the request
+ * @return the ID of the request
+ *
+ * @since 1.0.0
+ */
+fun TextDocumentJsonRpcServer.codeAction(
+    params: CodeActionParams,
+    responseHandler: (TypedResponseObject<List<Command>, JsonElement>.() -> Unit)? = null
+): JsonIntOrString = server.sendRequest(
+    method = TextDocumentRequest.CODE_ACTION,
+    params = CodeActionParams.serializeToJson(params),
+    responseHandler = responseHandler,
+    responseObjectConverter = CommandResponse
+)
+
+/**
+ * The code action request is sent from the client to the server to compute commands for
+ * a given text document and range.
+ *
+ * The request is trigger when the user moves the cursor into an problem marker in the
+ * editor or presses the lightbulb associated with a marker.
+ *
+ * @param textDocument the document in which the command was invoked
+ * @param range the range for which the command was invoked
+ * @param context context carrying additional information
+ * @param responseHandler the callback to process the response for the request
+ * @return the ID of the request
+ *
+ * @since 1.0.0
+ */
+fun TextDocumentJsonRpcServer.codeAction(
+    textDocument: TextDocumentIdentifier,
+    range: Range,
+    context: CodeActionContext,
+    responseHandler: (TypedResponseObject<List<Command>, JsonElement>.() -> Unit)? = null
+): JsonIntOrString = codeAction(
+    params = CodeActionParams(
+        textDocument = textDocument,
+        range = range,
+        context = context
+    ),
+    responseHandler = responseHandler
+)
--- src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/textDocument/TextDocument.kt
+++ src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/textDocument/TextDocument.kt
@@ -2215,5 +2215,251 @@ class TextDocumentDSL {
         )
     }
 
+    @Test
+    @DisplayName("supports sending textDocument/codeAction requests using parameter objects")
+    fun supports_sending_code_action_requests_using_parameter_objects() = testJsonRpc {
+        val id = client.textDocument.codeAction(
+            params = CodeActionParams(
+                textDocument = TextDocumentIdentifier(
+                    uri = "file:///home/lorem/ipsum.py"
+                ),
+                range = Range(
+                    start = Position(5u, 12u),
+                    end = Position(5u, 21u)
+                ),
+                context = CodeActionContext(
+                    diagnostics = listOf()
+                )
+            )
+        )
+        assertEquals(JsonIntOrString.IntegerValue(1), id)
+
+        assertEquals(
+            jsonObjectOf(
+                "jsonrpc" to JsonPrimitive("2.0"),
+                "method" to JsonPrimitive("textDocument/codeAction"),
+                "id" to JsonPrimitive(1),
+                "params" to jsonObjectOf(
+                    "textDocument" to jsonObjectOf(
+                        "uri" to JsonPrimitive("file:///home/lorem/ipsum.py")
+                    ),
+                    "range" to jsonObjectOf(
+                        "start" to jsonObjectOf(
+                            "line" to JsonPrimitive(5),
+                            "character" to JsonPrimitive(12)
+                        ),
+                        "end" to jsonObjectOf(
+                            "line" to JsonPrimitive(5),
+                            "character" to JsonPrimitive(21)
+                        )
+                    ),
+                    "context" to jsonObjectOf(
+                        "diagnostics" to jsonArrayOf()
+                    )
+                )
+            ),
+            server.receive()
+        )
+    }
+
+    @Test
+    @DisplayName("supports textDocument/codeAction request callback receiving a result using parameter objects")
+    fun supports_code_action_request_callback_receiving_a_result_using_parameter_objects() = testJsonRpc {
+        var called = 0
+
+        client.textDocument.codeAction(
+            params = CodeActionParams(
+                textDocument = TextDocumentIdentifier(
+                    uri = "file:///home/lorem/ipsum.py"
+                ),
+                range = Range(
+                    start = Position(5u, 12u),
+                    end = Position(5u, 21u)
+                ),
+                context = CodeActionContext(
+                    diagnostics = listOf()
+                )
+            )
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(null, error)
+        }
+
+        server.jsonRpc {
+            request {
+                textDocument.codeAction {
+                    listOf()
+                }
+            }
+        }
+
+        assertEquals(0, called, "The textDocument.codeAction DSL handler should not have been called.")
+        client.jsonRpc {} // The "textDocument/codeAction" response is processed by the handler callback.
+        assertEquals(1, called, "The textDocument.codeAction DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports textDocument/codeAction request callback receiving an error using parameter objects")
+    fun supports_code_action_request_callback_receiving_an_error_using_parameter_objects() = testJsonRpc {
+        var called = 0
+
+        client.textDocument.codeAction(
+            params = CodeActionParams(
+                textDocument = TextDocumentIdentifier(
+                    uri = "file:///home/lorem/ipsum.py"
+                ),
+                range = Range(
+                    start = Position(5u, 12u),
+                    end = Position(5u, 21u)
+                ),
+                context = CodeActionContext(
+                    diagnostics = listOf()
+                )
+            )
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(ErrorCodes.InternalError, error?.code)
+            assertEquals("Lorem ipsum", error?.message)
+        }
+
+        server.jsonRpc {
+            request {
+                textDocument.codeAction {
+                    throw InternalError(message = "Lorem ipsum")
+                }
+            }
+        }
+
+        assertEquals(0, called, "The textDocument.codeAction DSL handler should not have been called.")
+        client.jsonRpc {} // The "textDocument/codeAction" response is processed by the handler callback.
+        assertEquals(1, called, "The textDocument.codeAction DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports sending textDocument/codeAction requests using function parameters")
+    fun supports_sending_code_action_requests_using_function_parameters() = testJsonRpc {
+        val id = client.textDocument.codeAction(
+            textDocument = TextDocumentIdentifier(
+                uri = "file:///home/lorem/ipsum.py"
+            ),
+            range = Range(
+                start = Position(5u, 12u),
+                end = Position(5u, 21u)
+            ),
+            context = CodeActionContext(
+                diagnostics = listOf()
+            )
+        )
+        assertEquals(JsonIntOrString.IntegerValue(1), id)
+
+        assertEquals(
+            jsonObjectOf(
+                "jsonrpc" to JsonPrimitive("2.0"),
+                "method" to JsonPrimitive("textDocument/codeAction"),
+                "id" to JsonPrimitive(1),
+                "params" to jsonObjectOf(
+                    "textDocument" to jsonObjectOf(
+                        "uri" to JsonPrimitive("file:///home/lorem/ipsum.py")
+                    ),
+                    "range" to jsonObjectOf(
+                        "start" to jsonObjectOf(
+                            "line" to JsonPrimitive(5),
+                            "character" to JsonPrimitive(12)
+                        ),
+                        "end" to jsonObjectOf(
+                            "line" to JsonPrimitive(5),
+                            "character" to JsonPrimitive(21)
+                        )
+                    ),
+                    "context" to jsonObjectOf(
+                        "diagnostics" to jsonArrayOf()
+                    )
+                )
+            ),
+            server.receive()
+        )
+    }
+
+    @Test
+    @DisplayName("supports textDocument/codeAction request callback receiving a result using function parameters")
+    fun supports_code_action_request_callback_receiving_a_result_using_function_parameters() = testJsonRpc {
+        var called = 0
+
+        client.textDocument.codeAction(
+            textDocument = TextDocumentIdentifier(
+                uri = "file:///home/lorem/ipsum.py"
+            ),
+            range = Range(
+                start = Position(5u, 12u),
+                end = Position(5u, 21u)
+            ),
+            context = CodeActionContext(
+                diagnostics = listOf()
+            )
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(null, error)
+        }
+
+        server.jsonRpc {
+            request {
+                textDocument.codeAction {
+                    listOf()
+                }
+            }
+        }
+
+        assertEquals(0, called, "The textDocument.codeAction DSL handler should not have been called.")
+        client.jsonRpc {} // The "textDocument/codeAction" response is processed by the handler callback.
+        assertEquals(1, called, "The textDocument.codeAction DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports textDocument/codeAction request callback receiving an error using function parameters")
+    fun supports_code_action_request_callback_receiving_an_error_using_function_parameters() = testJsonRpc {
+        var called = 0
+
+        client.textDocument.codeAction(
+            textDocument = TextDocumentIdentifier(
+                uri = "file:///home/lorem/ipsum.py"
+            ),
+            range = Range(
+                start = Position(5u, 12u),
+                end = Position(5u, 21u)
+            ),
+            context = CodeActionContext(
+                diagnostics = listOf()
+            )
+        ) {
+            ++called
+
+            assertEquals(0, result.size)
+
+            assertEquals(ErrorCodes.InternalError, error?.code)
+            assertEquals("Lorem ipsum", error?.message)
+        }
+
+        server.jsonRpc {
+            request {
+                textDocument.codeAction {
+                    throw InternalError(message = "Lorem ipsum")
+                }
+            }
+        }
+
+        assertEquals(0, called, "The textDocument.codeAction DSL handler should not have been called.")
+        client.jsonRpc {} // The "textDocument/codeAction" response is processed by the handler callback.
+        assertEquals(1, called, "The textDocument.codeAction DSL handler should have been called.")
+    }
+
     // endregion
 }
