adce11508b9bfab391e93e63afc375f90100eac2
Reece H. Dunn <msclrhd@gmail.com>
2023-04-07 13:29:53 +0100

    Add a codeLens/resolve send request API

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/codeLens/Resolve.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/codeLens/Resolve.kt
@@ -1,9 +1,38 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.lsp.codeLens
 
+import kotlinx.serialization.json.JsonElement
 import xqt.kotlinx.lsp.textDocument.CodeLens
-import xqt.kotlinx.rpc.json.protocol.params
-import xqt.kotlinx.rpc.json.protocol.sendResult
+import xqt.kotlinx.rpc.json.protocol.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonIntOrString
+
+/**
+ * The response of a code lens request.
+ *
+ * @param id the request id
+ * @param result the result of the request
+ * @param error the error object in case the request failed
+ * @param jsonrpc the version of the JSON-RPC protocol
+ *
+ * @since 1.0.0
+ */
+data class ResolveResponse(
+    override val id: JsonIntOrString?,
+    override val result: CodeLens?,
+    override val error: TypedErrorObject<JsonElement>?,
+    override val jsonrpc: String
+) : TypedResponseObject<CodeLens?, JsonElement> {
+    companion object : TypedResponseObjectConverter<CodeLens?, JsonElement> {
+        override fun convert(response: ResponseObject): TypedResponseObject<CodeLens?, JsonElement> {
+            return ResolveResponse(
+                id = response.id,
+                result = response.result?.let { CodeLens.deserialize(it) },
+                error = response.error,
+                jsonrpc = response.jsonrpc
+            )
+        }
+    }
+}
 
 /**
  * The code lens resolve request is sent from the client to the server to resolve the
@@ -17,3 +46,23 @@ fun CodeLensRequest.resolve(handler: CodeLens.() -> CodeLens) {
         request.sendResult(result, CodeLens)
     }
 }
+
+/**
+ * The code lens resolve request is sent from the client to the server to resolve the
+ * command for a given code lens item.
+ *
+ * @param params the request parameters
+ * @param responseHandler the callback to process the response for the request
+ * @return the ID of the request
+ *
+ * @since 1.0.0
+ */
+fun CodeLensJsonRpcServer.resolve(
+    params: CodeLens,
+    responseHandler: (TypedResponseObject<CodeLens?, JsonElement>.() -> Unit)? = null
+): JsonIntOrString = server.sendRequest(
+    method = CodeLensRequest.RESOLVE,
+    params = CodeLens.serializeToJson(params),
+    responseHandler = responseHandler,
+    responseObjectConverter = ResolveResponse
+)
--- src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/codeLens/CodeLens.kt
+++ src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/codeLens/CodeLens.kt
@@ -2,9 +2,14 @@
 package xqt.kotlinx.lsp.test.codeLens
 
 import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.lsp.base.ErrorCodes
+import xqt.kotlinx.lsp.base.InternalError
 import xqt.kotlinx.lsp.codeLens.codeLens
 import xqt.kotlinx.lsp.codeLens.resolve
 import xqt.kotlinx.lsp.test.base.testJsonRpc
+import xqt.kotlinx.lsp.textDocument.CodeLens
+import xqt.kotlinx.lsp.types.Position
+import xqt.kotlinx.lsp.types.Range
 import xqt.kotlinx.rpc.json.protocol.jsonRpc
 import xqt.kotlinx.rpc.json.protocol.request
 import xqt.kotlinx.rpc.json.serialization.jsonObjectOf
@@ -76,6 +81,112 @@ class TextCodeLensDSL {
             ),
             client.receive()
         )
+    }
+
+    @Test
+    @DisplayName("supports sending codeLens/resolve requests using parameter objects")
+    fun supports_sending_document_symbol_requests_using_parameter_objects() = testJsonRpc {
+        val id = client.codeLens.resolve(
+            params = CodeLens(
+                range = Range(
+                    start = Position(5u, 12u),
+                    end = Position(5u, 21u)
+                )
+            )
+        )
+        assertEquals(JsonIntOrString.IntegerValue(1), id)
+
+        assertEquals(
+            jsonObjectOf(
+                "jsonrpc" to JsonPrimitive("2.0"),
+                "method" to JsonPrimitive("codeLens/resolve"),
+                "id" to JsonPrimitive(1),
+                "params" to jsonObjectOf(
+                    "range" to jsonObjectOf(
+                        "start" to jsonObjectOf(
+                            "line" to JsonPrimitive(5),
+                            "character" to JsonPrimitive(12)
+                        ),
+                        "end" to jsonObjectOf(
+                            "line" to JsonPrimitive(5),
+                            "character" to JsonPrimitive(21)
+                        )
+                    )
+                )
+            ),
+            server.receive()
+        )
+    }
+
+    @Test
+    @DisplayName("supports codeLens/resolve request callback receiving a result using parameter objects")
+    fun supports_document_symbol_request_callback_receiving_a_result_using_parameter_objects() = testJsonRpc {
+        var called = 0
+
+        client.codeLens.resolve(
+            params = CodeLens(
+                range = Range(
+                    start = Position(5u, 12u),
+                    end = Position(5u, 21u)
+                )
+            )
+        ) {
+            ++called
+
+            assertEquals(Position(5u, 12u), result?.range?.start)
+            assertEquals(Position(5u, 21u), result?.range?.end)
+
+            assertEquals(null, result?.command)
+            assertEquals(null, result?.data)
+
+            assertEquals(null, error)
+        }
+
+        server.jsonRpc {
+            request {
+                codeLens.resolve {
+                    this
+                }
+            }
+        }
+
+        assertEquals(0, called, "The codeLens.resolve DSL handler should not have been called.")
+        client.jsonRpc {} // The "codeLens/resolve" response is processed by the handler callback.
+        assertEquals(1, called, "The codeLens.resolve DSL handler should have been called.")
+    }
+
+    @Test
+    @DisplayName("supports codeLens/resolve request callback receiving an error using parameter objects")
+    fun supports_document_symbol_request_callback_receiving_an_error_using_parameter_objects() = testJsonRpc {
+        var called = 0
+
+        client.codeLens.resolve(
+            params = CodeLens(
+                range = Range(
+                    start = Position(5u, 12u),
+                    end = Position(5u, 21u)
+                )
+            )
+        ) {
+            ++called
+
+            assertEquals(null, result)
+
+            assertEquals(ErrorCodes.InternalError, error?.code)
+            assertEquals("Lorem ipsum", error?.message)
+        }
+
+        server.jsonRpc {
+            request {
+                codeLens.resolve {
+                    throw InternalError(message = "Lorem ipsum")
+                }
+            }
+        }
+
+        assertEquals(0, called, "The codeLens.resolve DSL handler should not have been called.")
+        client.jsonRpc {} // The "codeLens/resolve" response is processed by the handler callback.
+        assertEquals(1, called, "The codeLens.resolve DSL handler should have been called.")
     }
 
     // endregion
