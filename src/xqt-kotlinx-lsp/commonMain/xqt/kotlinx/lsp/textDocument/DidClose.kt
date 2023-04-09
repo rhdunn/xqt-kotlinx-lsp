49c0357e20a722a0d978471395b6c5f17969d2ed
Reece H. Dunn <msclrhd@gmail.com>
2023-04-09 12:05:01 +0100

    DidCloseTextDocumentParams

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/DidClose.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/DidClose.kt
@@ -1,9 +1,39 @@
 // Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
 package xqt.kotlinx.lsp.textDocument
 
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
 import xqt.kotlinx.lsp.types.TextDocumentIdentifier
 import xqt.kotlinx.rpc.json.protocol.method
 import xqt.kotlinx.rpc.json.protocol.sendNotification
+import xqt.kotlinx.rpc.json.serialization.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonString
+
+/**
+ * The `textDocument/didClose` request parameters.
+ *
+ * @since 2.0.0
+ */
+data class DidCloseTextDocumentParams(
+    /**
+     * The document that was closed.
+     */
+    val textDocument: TextDocumentIdentifier
+) {
+    companion object : JsonSerialization<DidCloseTextDocumentParams> {
+        override fun serializeToJson(value: DidCloseTextDocumentParams): JsonObject = buildJsonObject {
+            put("textDocument", value.textDocument, TextDocumentIdentifier)
+        }
+
+        override fun deserialize(json: JsonElement): DidCloseTextDocumentParams = when (json) {
+            !is JsonObject -> unsupportedKindType(json)
+            else -> DidCloseTextDocumentParams(
+                textDocument = json.get("textDocument", TextDocumentIdentifier)
+            )
+        }
+    }
+}
 
 /**
  * The document close notification is sent from the client to the server when the document
