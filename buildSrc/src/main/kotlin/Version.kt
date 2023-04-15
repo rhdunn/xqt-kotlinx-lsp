cb2ea40c18ada0dc3587227117bf9cdc182f977d
Reece H. Dunn <msclrhd@gmail.com>
2023-04-15 16:00:54 +0100

    Add a Version.Plugin.kotlinMultiplatform constant.

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,3 +1,7 @@
 object Version {
     const val kotlin = "1.7.20"
+
+    object Plugin {
+        const val kotlinMultiplatform = kotlin
+    }
 }
--- src/xqt-kotlinx-lsp/build.gradle.kts
+++ src/xqt-kotlinx-lsp/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.7.20"
+    kotlin("multiplatform") version Version.Plugin.kotlinMultiplatform
     kotlin("plugin.serialization") version "1.7.20"
     id("maven-publish")
 }
