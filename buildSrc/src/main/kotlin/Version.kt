dd7bc7c900cec82d9379bb54bbba3d89bcf53995
Reece H. Dunn <msclrhd@gmail.com>
2023-04-23 20:31:00 +0100

    Add a Version.Kotlin constant.

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,11 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Versions of the various plugins and libraries used by the project.
+ */
+object Version {
+    /**
+     * The version of the Kotlin compiler and runtime.
+     */
+    const val Kotlin = "1.7.20"
+}
