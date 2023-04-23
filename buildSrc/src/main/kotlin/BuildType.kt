d905142830daf20b4c40fc276a3dcafc7a6ccad1
Reece H. Dunn <msclrhd@gmail.com>
2023-04-23 15:40:31 +0100

    Add a ProjectMetadata.Build property.

---
+++ buildSrc/src/main/kotlin/BuildType.kt
@@ -0,0 +1,18 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
+/**
+ * The build type associated with the project version.
+ *
+ * @param suffix The suffix applied to the version tag.
+ */
+enum class BuildType(val suffix: String) {
+    /**
+     * A release build.
+     */
+    Release(""),
+
+    /**
+     * A development snapshot build.
+     */
+    Snapshot("-SNAPSHOT"),
+}
--- buildSrc/src/main/kotlin/ProjectMetadata.kt
+++ buildSrc/src/main/kotlin/ProjectMetadata.kt
@@ -8,4 +8,24 @@ object ProjectMetadata {
      * A short description of the project.
      */
     const val Description = "Kotlin multiplatform project template" // TODO: Modify this property.
+
+    /**
+     * Version information about the current build of the project.
+     */
+    object Build {
+        /**
+         * The semantic version of the current version.
+         */
+        const val VersionTag = "1.0.0" // TODO: Modify this property.
+
+        /**
+         * The build type of this project.
+         */
+        val Type = BuildType.Snapshot // TODO: Modify this property.
+
+        /**
+         * The artifact version ID.
+         */
+        val Version = "$VersionTag${Type.suffix}"
+    }
 }
