e249f30843c85146992f4d4ab7f6c17d236690ad
Reece H. Dunn <msclrhd@gmail.com>
2023-04-23 13:19:06 +0100

    Add a nativeMain accessor property.

---
+++ buildSrc/src/main/kotlin/KotlinNative.kt
@@ -0,0 +1,8 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+import org.gradle.api.NamedDomainObjectContainer
+
+/**
+ * Access the native main configuration object.
+ */
+val <T> NamedDomainObjectContainer<T>.nativeMain: T
+    get() = findByName("nativeMain")!!
