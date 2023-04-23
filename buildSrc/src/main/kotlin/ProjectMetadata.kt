5037647da4c327d07c66cc4a5a153fdd56c0b8ea
Reece H. Dunn <msclrhd@gmail.com>
2023-04-23 15:36:06 +0100

    Add a ProjectMetadata.Description property.

---
+++ buildSrc/src/main/kotlin/ProjectMetadata.kt
@@ -0,0 +1,11 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Information about the project.
+ */
+object ProjectMetadata {
+    /**
+     * A short description of the project.
+     */
+    const val Description = "Kotlin multiplatform project template" // TODO: Modify this property.
+}
