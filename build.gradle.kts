4e600086fc6851b4ee947a612808290a33fe2eac
Reece H. Dunn <msclrhd@gmail.com>
2023-04-30 14:52:19 +0100

    Fix 'Nothing to document' when building the dokka documentation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,13 @@ allprojects {
 }
 
 plugins {
+    kotlin("multiplatform") version Version.Plugin.KotlinMultiplatform
     id("org.jetbrains.dokka") version Version.Plugin.Dokka
+}
+
+// Workaround for generating documentation. The root project needs kotlin to be
+// configured for at least one multiplatform target, otherwise dokka will report
+// "Nothing to document" in the dokkaHtml and dokkaHtmlPartial tasks.
+kotlin {
+    jvm()
 }
