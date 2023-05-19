bae17865404c47d95bbd40d3f100a1a42816653a
Reece H. Dunn <msclrhd@gmail.com>
2023-05-19 09:29:30 +0100

    DocumentLink

--- src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/DocumentLink.kt
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/textDocument/DocumentLink.kt
@@ -4,8 +4,10 @@ package xqt.kotlinx.lsp.textDocument
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
+import xqt.kotlinx.lsp.types.Range
 import xqt.kotlinx.lsp.types.TextDocumentIdentifier
 import xqt.kotlinx.rpc.json.serialization.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonString
 
 /**
  * Parameters for `textDocument/documentLink` request.
@@ -27,6 +29,39 @@ data class DocumentLinkParams(
             !is JsonObject -> unsupportedKindType(json)
             else -> DocumentLinkParams(
                 textDocument = json.get("textDocument", TextDocumentIdentifier)
+            )
+        }
+    }
+}
+
+/**
+ * A document link is a range in a text document that links to an internal or
+ * external resource, like another text document or a website.
+ *
+ * @since 2.0.0
+ */
+data class DocumentLink(
+    /**
+     * The range this link applies to.
+     */
+    val range: Range,
+
+    /**
+     * The uri this link points to.
+     */
+    val target: String
+) {
+    companion object : JsonSerialization<DocumentLink> {
+        override fun serializeToJson(value: DocumentLink): JsonObject = buildJsonObject {
+            put("range", value.range, Range)
+            put("target", value.target, JsonString)
+        }
+
+        override fun deserialize(json: JsonElement): DocumentLink = when (json) {
+            !is JsonObject -> unsupportedKindType(json)
+            else -> DocumentLink(
+                range = json.get("range", Range),
+                target = json.get("target", JsonString)
             )
         }
     }
---
+++ src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/textDocument/DocumentLink.kt
@@ -0,0 +1,70 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.test.textDocument
+
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.lsp.textDocument.DocumentLink
+import xqt.kotlinx.lsp.types.Position
+import xqt.kotlinx.rpc.json.serialization.UnsupportedKindTypeException
+import xqt.kotlinx.rpc.json.serialization.jsonArrayOf
+import xqt.kotlinx.rpc.json.serialization.jsonObjectOf
+import xqt.kotlinx.test.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+@DisplayName("The DocumentLink type")
+class TheDocumentLinkType {
+    @Test
+    @DisplayName("supports the non-optional properties")
+    fun supports_the_non_optional_properties() {
+        val json = jsonObjectOf(
+            "range" to jsonObjectOf(
+                "start" to jsonObjectOf(
+                    "line" to JsonPrimitive(2),
+                    "character" to JsonPrimitive(6)
+                ),
+                "end" to jsonObjectOf(
+                    "line" to JsonPrimitive(2),
+                    "character" to JsonPrimitive(8)
+                )
+            ),
+            "target" to JsonPrimitive("file:///home/lorem/ipsum.py")
+        )
+
+        val link = DocumentLink.deserialize(json)
+        assertEquals(Position(2u, 6u), link.range.start)
+        assertEquals(Position(2u, 8u), link.range.end)
+        assertEquals("file:///home/lorem/ipsum.py", link.target)
+
+        assertEquals(json, DocumentLink.serializeToJson(link))
+    }
+
+    @Test
+    @DisplayName("throws an error if the kind type is not supported")
+    fun throws_an_error_if_the_kind_type_is_not_supported() {
+        val e1 = assertFails { DocumentLink.deserialize(jsonArrayOf()) }
+        assertEquals(UnsupportedKindTypeException::class, e1::class)
+        assertEquals("Unsupported kind type 'array'", e1.message)
+
+        val e2 = assertFails { DocumentLink.deserialize(JsonNull) }
+        assertEquals(UnsupportedKindTypeException::class, e2::class)
+        assertEquals("Unsupported kind type 'null'", e2.message)
+
+        val e3 = assertFails { DocumentLink.deserialize(JsonPrimitive("test")) }
+        assertEquals(UnsupportedKindTypeException::class, e3::class)
+        assertEquals("Unsupported kind type 'string'", e3.message)
+
+        val e4 = assertFails { DocumentLink.deserialize(JsonPrimitive(true)) }
+        assertEquals(UnsupportedKindTypeException::class, e4::class)
+        assertEquals("Unsupported kind type 'boolean'", e4.message)
+
+        val e5 = assertFails { DocumentLink.deserialize(JsonPrimitive(1)) }
+        assertEquals(UnsupportedKindTypeException::class, e5::class)
+        assertEquals("Unsupported kind type 'integer'", e5.message)
+
+        val e6 = assertFails { DocumentLink.deserialize(JsonPrimitive(1.2)) }
+        assertEquals(UnsupportedKindTypeException::class, e6::class)
+        assertEquals("Unsupported kind type 'decimal'", e6.message)
+    }
+}
