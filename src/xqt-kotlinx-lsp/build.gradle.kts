9a4af5685825d9f78244fbd6fd51663c80eb03ce
Reece H. Dunn <msclrhd@gmail.com>
2023-05-22 09:26:34 +0100

    Update the Kotlin/JS and Kotlin/Native targets for Kotlin 1.8.21.

--- src/xqt-kotlinx-lsp/build.gradle.kts
+++ src/xqt-kotlinx-lsp/build.gradle.kts
@@ -101,7 +101,7 @@ rootProject.plugins.withType<NodeJsRootPlugin> {
     rootProject.the<NodeJsRootExtension>().download = BuildConfiguration.nodeJsDownload(project)
 }
 
-kotlin.js(KotlinJsCompilerType.BOTH).browser {
+kotlin.js(KotlinJsCompilerType.IR).browser {
     testTask {
         useKarma {
             when (BuildConfiguration.karmaBrowserTarget(project)) {
@@ -128,7 +128,7 @@ kotlin.js(KotlinJsCompilerType.BOTH).browser {
     }
 }
 
-kotlin.js(KotlinJsCompilerType.BOTH).nodejs {
+kotlin.js(KotlinJsCompilerType.IR).nodejs {
 }
 
 kotlin.sourceSets {
@@ -152,29 +152,23 @@ ProjectMetadata.BuildTargets.KonanTargets.forEach { konanTarget ->
         KonanTarget.ANDROID_ARM64 -> kotlin.androidNativeArm64(nativeName) // Tier 3
         KonanTarget.ANDROID_X64 -> kotlin.androidNativeX64(nativeName) // Tier 3
         KonanTarget.ANDROID_X86 -> kotlin.androidNativeX86(nativeName) // Tier 3
-        KonanTarget.IOS_ARM32 -> kotlin.iosArm32(nativeName) // Deprecated, to be removed in 1.9.20
         KonanTarget.IOS_ARM64 -> kotlin.iosArm64(nativeName) // Tier 2
         KonanTarget.IOS_SIMULATOR_ARM64 -> kotlin.iosSimulatorArm64(nativeName) // Tier 1
         KonanTarget.IOS_X64 -> kotlin.iosX64(nativeName) // Tier 1
-        KonanTarget.LINUX_ARM32_HFP -> kotlin.linuxArm32Hfp(nativeName) // Deprecated, to be removed in 1.9.20
         KonanTarget.LINUX_ARM64 -> kotlin.linuxArm64(nativeName) // Tier 2
-        KonanTarget.LINUX_MIPS32 -> kotlin.linuxMips32(nativeName) // Deprecated, to be removed in 1.9.20
-        KonanTarget.LINUX_MIPSEL32 -> kotlin.linuxMipsel32(nativeName) // Deprecated, to be removed in 1.9.20
         KonanTarget.LINUX_X64 -> kotlin.linuxX64(nativeName) // Tier 1 ; native host
         KonanTarget.MACOS_ARM64 -> kotlin.macosArm64(nativeName) // Tier 1 ; native host
         KonanTarget.MACOS_X64 -> kotlin.macosX64(nativeName) // Tier 1 ; native host
         KonanTarget.MINGW_X64 -> kotlin.mingwX64(nativeName) // Tier 3 ; native host
-        KonanTarget.MINGW_X86 -> kotlin.mingwX86(nativeName) // Deprecated, to be removed in 1.9.20
         KonanTarget.TVOS_ARM64 -> kotlin.tvosArm64(nativeName) // Tier 2
         KonanTarget.TVOS_SIMULATOR_ARM64 -> kotlin.tvosSimulatorArm64(nativeName) // Tier 2
         KonanTarget.TVOS_X64 -> kotlin.tvosX64(nativeName) // Tier 2
-        KonanTarget.WASM32 -> kotlin.wasm32(nativeName) // Deprecated, to be removed in 1.9.20
         KonanTarget.WATCHOS_ARM32 -> kotlin.watchosArm32(nativeName) // Tier 2
         KonanTarget.WATCHOS_ARM64 -> kotlin.watchosArm64(nativeName) // Tier 2
+        KonanTarget.WATCHOS_DEVICE_ARM64 -> kotlin.watchosDeviceArm64(nativeName) // Tier 3
         KonanTarget.WATCHOS_SIMULATOR_ARM64 -> kotlin.watchosSimulatorArm64(nativeName) // Tier 2
         KonanTarget.WATCHOS_X64 -> kotlin.watchosX64(nativeName) // Tier 2
-        KonanTarget.WATCHOS_X86 -> kotlin.watchosX86(nativeName) // Deprecated, to be removed in 1.9.20
-        is KonanTarget.ZEPHYR -> throw GradleException("Kotlin/Native build target 'zephyr' is not supported.")
+        else -> throw GradleException("Kotlin/Native build target '$konanTarget' is not supported.")
     }
 
     if (konanTarget == konanBuildTarget)
