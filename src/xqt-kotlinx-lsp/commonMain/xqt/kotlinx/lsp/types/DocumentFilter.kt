d73339e809a01cfdd0522f71b1fba89c4d9c3d8e
Reece H. Dunn <msclrhd@gmail.com>
2023-05-31 08:42:11 +0100

    DocumentFilter

---
+++ src/xqt-kotlinx-lsp/commonMain/xqt/kotlinx/lsp/types/DocumentFilter.kt
@@ -0,0 +1,48 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.types
+
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import xqt.kotlinx.rpc.json.serialization.*
+import xqt.kotlinx.rpc.json.serialization.types.JsonString
+
+/**
+ * A document filter denotes a document through properties like `language`,
+ * `schema` or `pattern`.
+ *
+ * @since 3.0.0
+ */
+data class DocumentFilter(
+    /**
+     * A language id, like `typescript`.
+     */
+    val language: String? = null,
+
+    /**
+     * A Uri scheme, like `file` or `untitled`.
+     */
+    val scheme: String? = null,
+
+    /**
+     * A glob pattern, like `*.{ts,js}`.
+     */
+    val pattern: String? = null
+) {
+    companion object : JsonSerialization<DocumentFilter> {
+        override fun serializeToJson(value: DocumentFilter): JsonObject = buildJsonObject {
+            putOptional("language", value.language, JsonString)
+            putOptional("scheme", value.scheme, JsonString)
+            putOptional("pattern", value.pattern, JsonString)
+        }
+
+        override fun deserialize(json: JsonElement): DocumentFilter = when (json) {
+            !is JsonObject -> unsupportedKindType(json)
+            else -> DocumentFilter(
+                language = json.getOptional("language", JsonString),
+                scheme = json.getOptional("scheme", JsonString),
+                pattern = json.getOptional("pattern", JsonString)
+            )
+        }
+    }
+}
---
+++ src/xqt-kotlinx-lsp/commonTest/xqt/kotlinx/lsp/test/types/DocumentFilter.kt
@@ -0,0 +1,102 @@
+// Copyright (C) 2023 Reece H. Dunn. SPDX-License-Identifier: Apache-2.0
+package xqt.kotlinx.lsp.test.types
+
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonPrimitive
+import xqt.kotlinx.lsp.types.DocumentFilter
+import xqt.kotlinx.rpc.json.serialization.UnsupportedKindTypeException
+import xqt.kotlinx.rpc.json.serialization.jsonArrayOf
+import xqt.kotlinx.rpc.json.serialization.jsonObjectOf
+import xqt.kotlinx.test.DisplayName
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+@DisplayName("The DocumentFilter type")
+class TheDocumentFilterType {
+    @Test
+    @DisplayName("supports the non-optional properties")
+    fun supports_the_non_optional_properties() {
+        val json = jsonObjectOf()
+
+        val filter = DocumentFilter.deserialize(json)
+        assertEquals(null, filter.language)
+        assertEquals(null, filter.scheme)
+        assertEquals(null, filter.pattern)
+
+        assertEquals(json, DocumentFilter.serializeToJson(filter))
+    }
+
+    @Test
+    @DisplayName("supports the language property")
+    fun supports_the_language_property() {
+        val json = jsonObjectOf(
+            "language" to JsonPrimitive("typescript")
+        )
+
+        val filter = DocumentFilter.deserialize(json)
+        assertEquals("typescript", filter.language)
+        assertEquals(null, filter.scheme)
+        assertEquals(null, filter.pattern)
+
+        assertEquals(json, DocumentFilter.serializeToJson(filter))
+    }
+
+    @Test
+    @DisplayName("supports the scheme property")
+    fun supports_the_scheme_property() {
+        val json = jsonObjectOf(
+            "scheme" to JsonPrimitive("file")
+        )
+
+        val filter = DocumentFilter.deserialize(json)
+        assertEquals(null, filter.language)
+        assertEquals("file", filter.scheme)
+        assertEquals(null, filter.pattern)
+
+        assertEquals(json, DocumentFilter.serializeToJson(filter))
+    }
+
+    @Test
+    @DisplayName("supports the pattern property")
+    fun supports_the_pattern_property() {
+        val json = jsonObjectOf(
+            "pattern" to JsonPrimitive("*.{ts,js}")
+        )
+
+        val filter = DocumentFilter.deserialize(json)
+        assertEquals(null, filter.language)
+        assertEquals(null, filter.scheme)
+        assertEquals("*.{ts,js}", filter.pattern)
+
+        assertEquals(json, DocumentFilter.serializeToJson(filter))
+    }
+
+    @Test
+    @DisplayName("throws an error if the kind type is not supported")
+    fun throws_an_error_if_the_kind_type_is_not_supported() {
+        val e1 = assertFails { DocumentFilter.deserialize(jsonArrayOf()) }
+        assertEquals(UnsupportedKindTypeException::class, e1::class)
+        assertEquals("Unsupported kind type 'array'", e1.message)
+
+        val e2 = assertFails { DocumentFilter.deserialize(JsonNull) }
+        assertEquals(UnsupportedKindTypeException::class, e2::class)
+        assertEquals("Unsupported kind type 'null'", e2.message)
+
+        val e3 = assertFails { DocumentFilter.deserialize(JsonPrimitive("test")) }
+        assertEquals(UnsupportedKindTypeException::class, e3::class)
+        assertEquals("Unsupported kind type 'string'", e3.message)
+
+        val e4 = assertFails { DocumentFilter.deserialize(JsonPrimitive(true)) }
+        assertEquals(UnsupportedKindTypeException::class, e4::class)
+        assertEquals("Unsupported kind type 'boolean'", e4.message)
+
+        val e5 = assertFails { DocumentFilter.deserialize(JsonPrimitive(1)) }
+        assertEquals(UnsupportedKindTypeException::class, e5::class)
+        assertEquals("Unsupported kind type 'integer'", e5.message)
+
+        val e6 = assertFails { DocumentFilter.deserialize(JsonPrimitive(1.2)) }
+        assertEquals(UnsupportedKindTypeException::class, e6::class)
+        assertEquals("Unsupported kind type 'decimal'", e6.message)
+    }
+}
